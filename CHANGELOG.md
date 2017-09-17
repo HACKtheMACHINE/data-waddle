f1744dd4d35e42aa7eb27e2f647aa7c72889c8fd
Daniel Brown <danielrbrownjr@gmail.com>
2017-09-17 11:51:02 -0400

    updated changelog, made template more general

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 # Changelog
 
-[![Version 0.1](https://img.shields.io/badge/V%200.1-Daniel-blue.svg)] Added first data processing program to project
+[![Version 0.1](https://img.shields.io/badge/V%200.1-Daniel-blue.svg)](./template.R) Added first data processing program to project
--- template.R
+++ template.R
@@ -18,20 +18,20 @@ dataset <- read.csv("dataset.csv") # imports data to data.frame
 
 ## Accounting for missing data #################################################
 # Taking care of missing data (If there is no missing data, do not use)
-dataset$annual.income <- ifelse(is.na(dataset$annual.income),
-                                ave(dataset$annual.income, 
+dataset$data.column3 <- ifelse(is.na(dataset$data.column3),
+                                ave(dataset$data.column3, 
                                     FUN = function(x) mean(x, na.rm = TRUE)),
-                                dataset$annual.income)
+                                dataset$data.column3)
 
 ## Scaling data features #######################################################
-unscaled <- select(dataset, gender, spending.score)
-dataset %<>% select(age, annual.income) # remove data we don't want to scale
+unscaled <- select(dataset, data.column1, data.column4)
+dataset %<>% select(data.column2, data.column3) # remove data we don't want to scale
 dataset %<>% scale # comment out if unnecessary
 dataset %<>% cbind(unscaled, .) # return data to data.frame
 rm(unscaled) # removes unscaled vector
 
 ## Handling categorical data ###################################################
-dataset$gender %<>% factor(levels = c('Male', 'Female'), labels = c(0, 1))
+dataset$data.column1 %<>% factor(levels = c('Male', 'Female'), labels = c(0, 1))
 
 ## Splitting data into training and testing sets ###############################
 set.seed(4) # ensures all parties get similar results
@@ -43,10 +43,10 @@ rm(split)
 ### Data manipulation ##########################################################
 ## Creating linear regression based on one independent variable ################
 y.pred <-
-  lm(formula = spending.score ~ age, data = data.train ) %>% 
+  lm(formula = data.column4 ~ data.column2, data = data.train ) %>% 
   predict(data.test)
 
 ### Data visualization #########################################################
 ## Creating a scatterplot based on some of the data ############################
-ggplot(dataset, aes(age, spending.score)) +
-  geom_point(size = dataset$annual.income, color = "Red")
+ggplot(dataset, aes(data.column2, data.column4)) +
+  geom_point(size = 4, color = "Red")
